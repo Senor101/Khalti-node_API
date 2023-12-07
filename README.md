f2bc51e5970b776a5f863fbe7e43430c54058b32
Senor101 <adikshyanta456@gmail.com>
2023-12-07 21:01:38 +0545

    sample request body

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ![Alt text](https://res.cloudinary.com/dsyoenswr/image/upload/v1701881087/p6hultnuro7jwc5tshuy.png)
 
-This project is a Nodejs based implementation for integrating a popular payment gateway in Nepal **Khalti** in your app.This project is designed to provide streamline online payment processing within your application. This can be either integrated in your own app or can be used as a microservice to suffice your application's need.
+This project is a Nodejs based implementation for integrating a popular payment gateway in Nepal, **Khalti** in your app.This project is designed to provide streamline online payment processing within your application. This can be either integrated in your own app or can be used as a microservice to suffice your application's need.
 
 ## Features
 - Simplified Integration
--- src/api/v1/controllers/payment.controller.ts
+++ src/api/v1/controllers/payment.controller.ts
@@ -1,6 +1,29 @@
-import { Request, Response, NextFunction } from "express"
+import { Request, Response, NextFunction } from "express";
+import request from "request";
 
 const payToMerch = async (req:Request, res:Response, next:NextFunction) => {
+    let options = {
+        "method" :"POST",
+        "url" : "",
+        "headers": {
+            "Authorization":"",
+            "Content-Type":"application/json",
+        },
+        body: JSON.stringify({
+            "return_url": "http://example.com/",
+            "website_url": "https://example.com/",
+            "amount": "1000",
+            "purchase_order_id": "Order01",
+            "purchase_order_name": "test",
+            "customer_info": {
+                "name": "Ram Bahadur",
+                "email": "test@khalti.com",
+                "phone": "9800000001"
+            }
+        })
+    }
+    const responseBody = request(options);
+    console.log(responseBody)
     return res.status(200).json({
         message: "Checking the controller."
     })
@@ -8,6 +31,7 @@ const payToMerch = async (req:Request, res:Response, next:NextFunction) => {
 
 const getMerch =async (req:Request, res:Response, next: NextFunction) => {
     try{
+
         return res.status(200).json({
             body:null
         })
