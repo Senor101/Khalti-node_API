b6c9126ca411453715efc52504bca2b8c69b2e59
Senor101 <adikshyanta456@gmail.com>
2023-12-09 16:44:50 +0545

    Pay with khalti view added

--- src/api/v1/controllers/payment.controller.ts
+++ src/api/v1/controllers/payment.controller.ts
@@ -1,18 +1,22 @@
 import { Request, Response, NextFunction } from "express";
 import request from "request";
+import dotenv from "dotenv";
+import axios from "axios";
+dotenv.config();
+
 
 const payToMerch = async (req:Request, res:Response, next:NextFunction) => {
+    const {amount} : {amount :number} = req.body;
     let options = {
         "method" :"POST",
-        "url" : "",
+        "url" : "https://khalti.com/api/v2/payment/verify/",
         "headers": {
-            "Authorization":"",
-            "Content-Type":"application/json",
+            "Authorization":`Key ${process.env.KHALTI_SECRET_KEY}`,
         },
-        body: JSON.stringify({
-            "return_url": "http://example.com/",
-            "website_url": "https://example.com/",
-            "amount": "1000",
+        data: JSON.stringify({
+            "return_url": "http://localhost:8000",
+            "website_url": "https://localhost:8000",
+            "amount": `${amount*100}`,
             "purchase_order_id": "Order01",
             "purchase_order_name": "test",
             "customer_info": {
@@ -22,10 +26,31 @@ const payToMerch = async (req:Request, res:Response, next:NextFunction) => {
             }
         })
     }
-    const responseBody = request(options);
-    console.log(responseBody)
+    request(options, function (error, response) {
+        if (error) throw new Error(error);
+        console.log(response.body);
+    });
+    // const responseBody = await axios.post(options.url,{
+    //     "return_url": "http://localhost:8000",
+    //     "website_url": "https://localhost:8000",
+    //     "amount": `${amount*100}`,
+    //     "purchase_order_id": "Order01",
+    //     "purchase_order_name": "test",
+    //     "customer_info": {
+    //         "name": "Ram Bahadur",
+    //         "email": "test@khalti.com",
+    //         "phone": "9800000001"
+    //     }
+    // },{
+    //     headers:{
+    //         "Content-Type": 'application/json',
+    //         "Authorization":`Key ${process.env.KHALTI_SECRET_KEY}`,
+    //     }
+    // });
+    // console.log(responseBody.data)
     return res.status(200).json({
-        message: "Checking the controller."
+        message: "Checking the controller.",
+        // data:responseBody.body
     })
 }
 
